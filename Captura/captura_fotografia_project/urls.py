3e0cace2132b017cac0dc96f314d639f1779f368
geraldacunasoftware <geraldacunasoftware@gmail.com>
2024-09-11 21:23:46 -0600

    Se agregaron los archivos base y links de bootstrap y google fonts

--- Captura/captura_fotografia_project/urls.py
+++ Captura/captura_fotografia_project/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from .settings import MEDIA_ROOT,MEDIA_URL
 from django.conf.urls.static import static
+from webapp.views import base
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',base),
 ]
 urlpatterns+=static(MEDIA_URL,document_root=MEDIA_ROOT)
