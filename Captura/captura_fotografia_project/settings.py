9f34a50ddbe86aa6bd61422e40395aab8181e7a2
geraldacunasoftware <geraldacunasoftware@gmail.com>
2024-10-11 05:34:29 -0600

    Update settings.py

--- Captura/captura_fotografia_project/settings.py
+++ Captura/captura_fotografia_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5bgebao@_giuh9&!7@ed0wk287mjm$_)*q6a)lt=jo3#aez3+5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.puravidawebs.com','puravidawebs.com','127.0.0.1','127.0.0.1:8000']
 
 
 # Application definition
